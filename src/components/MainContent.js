90eec588e3818792f8948540b210471956e1202d
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-04-09 14:23:16 -0300

    Time calculation moved and improved

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,70 +2,31 @@ import React, { useState } from "react"
 import "./MainContent.css"
 import Section from "./Section"
 import data from "../data.js"
-import { getCurrentTimeSeconds, convertTimeStringToSeconds } from "../utils"
 
 export default function MainContent () {
 
-    const [sections, setSections] = useState(data.sections)
-
     const [endingTime, setEndingTime] = useState("")
-    const [offsetTime, setOffsetTime] = useState(0)
-    function calculateOffsetTime(currentSectionId){
-        // A - get expected endtime
-        let expectedEndTime = convertTimeStringToSeconds(endingTime)
-        //console.log(`Expected Ending Time: ${expectedEndTime}`);
 
-        // B - get current time
-        let currentTime = getCurrentTimeSeconds()
-        //console.log(`Current Time: ${currentTime}`);
-
-        // C - get the amount of time expected to remain (add all left secions)
-        let timeShouldLeft = 0
-        for (var i = (currentSectionId - 1); i < data.sections.length; i++) {
-            timeShouldLeft += data.sections[currentSectionId].time
-        }
-        //console.log(`Time Should Left: ${timeShouldLeft}`);
-        
-        // D - subtract (A - B)
-        let timeReallyLeft = expectedEndTime - currentTime
-        //console.log(`Time Really Left: ${timeReallyLeft}`);
-
-        // E - compare this result with C (D - C)
-        setOffsetTime(timeReallyLeft - timeShouldLeft)
-
-        setSections(prev => prev)
-
-        //The E is the offset of time (for more or for less) and should be transformed in a number between -2 and 2 to change the color of SectionColorBar
-    }
-
-
-    //let totalTime = 0
+    //Create an Array of each sections time to pass to each section component for time calculations
+    let arrayOfSectionsTime = []
+    data.sections.forEach(element => {
+        arrayOfSectionsTime.push(element.time)
+    });
 
     //Get all the sections on the data json and create a section component for each
-    const sectionsElements = sections.map (x => {
+    const sectionsElements = data.sections.map (x => {
         //Add all section times to find the totalTime
-        //totalTime += x.time
         return (
-            <Section key={x.id} {...x} checkboxClicked={calculateOffsetTime} offsetTime={offsetTime}/>
+            <Section key={x.id} {...x} arrayOfSectionsTime={arrayOfSectionsTime} endingTime={endingTime}/>
         )
     })
-
-    //const totalTimeObj = convertSecondsToTimeObject(totalTime)
-    //convertTimeObjectToSeconds(totalTimeObj)
-
 
     return (
         <div className="maincontent-container">
             <h1 className="maincontent-title">{data.title}</h1>
-            {/* <div className="maincontent-time">
-                <input type="radio" name="timeType" id="radioTimeType1" checked/>
-                <p>Tempo Total de Discurso (HH/MM/SS):</p>
-                <input className="maincontent-totaltime-timepicker" type="time" step="1" readOnly={true} value={`${totalTimeObj.hours}:${totalTimeObj.minutes}:${totalTimeObj.seconds}`}/>
-            </div> */}
             <div className="maincontent-ending">
-                {/* <input type="radio" name="timeType" id="radioTimeType2"/> */}
                 <p>Hora de Encerramento (HH/MM/SS):</p>
-                <input className="maincontent-ending-timepicker" type="time" step="1" onChange={e => setEndingTime(e.target.value)}/>
+                <input className="maincontent-ending-timepicker" type="time" step="1" onChange={e => setEndingTime(e.target.value)} value={endingTime}/>
             </div>
             {sectionsElements}
         </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -3,8 +3,11 @@ import "./Section.css"
 import ArrowButton from "./ArrowButton"
 import SectionColorBar from "./SectionColorBar"
 import SectionTopic from "./SectionTopic"
+import { getCurrentTimeSeconds, convertTimeStringToSeconds } from "../utils"
 
 export default function Section (props) {
+
+    const [offsetTime, setOffsetTime] = useState(0)
 
     //To hide or show the content of this section
     const [isSectionContentShown, setIsContentShown] = useState(true)
@@ -21,33 +24,46 @@ export default function Section (props) {
 
     //Checkbox clicked
     const [isCheckboxClicked, setIsCheckboxClicked] = useState(false)
-    function chackboxClicked(){
+    
+    function calculateOffsetTime(){
+
         setIsCheckboxClicked(true)
-        props.checkboxClicked(props.id)
+
+        // A - get expected endtime
+        let expectedEndTime = convertTimeStringToSeconds(props.endingTime)
+
+        // B - get current time
+        let currentTime = getCurrentTimeSeconds()
+
+        // C - get the amount of time expected to remain (add all left secions)
+        let timeShouldLeft = 0
+        for (var i = (props.id - 1); i < props.arrayOfSectionsTime.length; i++) {
+            timeShouldLeft += props.arrayOfSectionsTime[i]
+        }
+        
+        // D - subtract (A - B)
+        let timeReallyLeft = expectedEndTime - currentTime
+
+        // E - compare this result with C (D - C)
+        //The E is the offset of time (for more or for less) and should be passed to SectionColorBar to change the color
+        setOffsetTime(timeReallyLeft - timeShouldLeft)
     }
 
     return (
         <div className="section-container">
-
             <div className="section-title">
-
-                <input className="section-checkbox" type="checkbox" onChange={chackboxClicked} disabled={isCheckboxClicked}></input> 
-
+                <input className="section-checkbox" name="section-checkbox" type="checkbox" onChange={calculateOffsetTime} checked={isCheckboxClicked} disabled={isCheckboxClicked}></input> 
                 <ArrowButton handleClick={toggleSectionContent} isActivated={isSectionContentShown} hasHiddentext="true"/>
-
                 <h2>{props.title}</h2>
-
             </div>
-
             {isSectionContentShown && 
                 <div className="section-content">
-                    <SectionColorBar offsetTime={props.offsetTime}/>
+                    <SectionColorBar offsetTime={offsetTime}/>
                     <div className="section-topics">
                         {topics}
                     </div>
                 </div>
             }
-
         </div>
     )
 }
