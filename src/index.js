fdaac9fb1712fc4a4dd7554c43da857174015ae5
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-04-19 16:51:13 -0300

    PWA make

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { register as registerServiceWorker } from './serviceWorkerRegistration'
 
 ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root'))
+registerServiceWorker();
